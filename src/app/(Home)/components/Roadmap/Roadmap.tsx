58320d6b90637e90c0c31e74c693dec5698149df
Naiem Foysal <naiemfoysal@Naiems-MacBook-Pro.local>
2023-12-03 22:47:27 +0600

    update-5

--- src/app/(Home)/components/Roadmap/Roadmap.tsx
+++ src/app/(Home)/components/Roadmap/Roadmap.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from "./roadmap.module.css"
 import Image from 'next/image'
 import Notification from '../Notifications/Notification'
-import Container from '@/components/Container/Container'
 import Title from '../Title/Title'
 
 const Roadmap = () => {
@@ -15,22 +14,17 @@ const Roadmap = () => {
           <div>
             <Image 
               className={styles.lineForRoadmapImg} 
-              width={200}
-              height={100} 
+              width={1321}
+              height={206} 
               layout="responsive" 
               src="/imgs/svg/lineForRoadmap.svg" 
               alt="" 
             />
-            {/* <Image className={styles.bee1Img} width={68} height={68} src="/imgs/svg/bee1.svg" alt="" /> */}
           </div>
-          {/* <Image className={styles.bee2Img} width={68} height={68} src="/imgs/svg/bee2.svg" alt="" /> */}
-          {/* <Image className={styles.bee3Img} width={68} height={68} src="/imgs/svg/bee3.svg" alt="" /> */}
-          
-          
-          
-          {/* <div>
-            <ul>
-              <div>Q4(2023)-Early Q1(2024)</div>
+          {/* Text for Big Screen */}
+          <div className={styles.contextContainerBig}>
+            <ul className={styles.ul1Big}>
+              <div className={styles.roadmapTitle}>Q4(2023)-Early Q1(2024)</div>
               <li>Private Sale</li>
               <li>Public Sale</li>
               <li>Certik Audit</li>
@@ -38,38 +32,26 @@ const Roadmap = () => {
               <li>Launch Web dApp</li>
               <li>Introduce Sniping Telegram Bot</li>
             </ul>
-
-            <ul>
-              <div>Q4(2023)-Early Q1(2024)</div>
-              <li>Private Sale</li>
-              <li>Public Sale</li>
-              <li>Certik Audit</li>
-              <li>List $HONEY on Uniswap & Pancakeswap</li>
-              <li>Launch Web dApp</li>
-              <li>Introduce Sniping Telegram Bot</li>
-            </ul>
-
-            <ul>
-              <div>Q4(2023)-Early Q1(2024)</div>
+            <ul className={styles.ul2Big}>
+              <div className={styles.roadmapTitle}>Q2(2024)</div>
               <li>Reach 10,000 Users</li>
               <li>Achieve 20,000 Holders</li>
               <li>Introduce Copy Snipe Feature</li>
             </ul>
-
-            
-            <ul>
-              <div>Early Q3(2024)</div>
+            <ul className={styles.ul3Big}>
+              <div className={styles.roadmapTitle}>Early Q3(2024)</div>
               <li>Support New Chains (Major Chains)</li>
               <li>Implement DeFi Solutions</li>
               <li>Introduce Copy Futures Trade on dApps (DYDX, GMX, etc.)</li>
             </ul>
-
-
-          </div> */}
+          </div>
+          
 
           
         </div>
         </div>
+
+        {/* Notification Text in Bottom */}
         <Notification 
           title="Liquidity" 
           desc="Honey Snipe's liquidity comprises 1,234,285 $Honey and $864,000 (launch price 0.7$), with an initial market cap of $2.4 million."
