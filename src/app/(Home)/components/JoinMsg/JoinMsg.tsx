4b29bd69afa2f6c7310c18224942fc8471aaaaa4
Naiem Foysal <naiemfoysal@Naiems-MacBook-Pro.local>
2023-12-16 00:43:10 +0600

    animation

--- src/app/(Home)/components/JoinMsg/JoinMsg.tsx
+++ src/app/(Home)/components/JoinMsg/JoinMsg.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
+"use client"
+import React, { useRef } from 'react'
 import styles from "./joinMsg.module.css"
 import SectionLayout from '../SectionLayout'
 import TextSection from '../TextSection'
+import hover3d from '@/utils/hover'
 
 const JoinMsg = () => {
+  const hero = useRef<HTMLDivElement>(null);
+  const hoverHero = hover3d(hero, {
+    x: 20,
+    y: -30,
+    z: 20,
+  });
+  const imageHover = hover3d(hero, {
+    x: 10,
+    y: 5,
+    z: 1,
+  });
   return (
-    <section className={styles.container}>
-      <div className={styles.joinMsg}>
+    <section style={{overflow:'hidden'}} ref={hero} className={styles.container}>
+      <div style={{
+                transform: hoverHero.transform,
+              }}>
+                <div style={{
+                  transform: imageHover.transform,
+                }} className={styles.joinMsg}>
         Join Honey Snipe and Unlock Limitless Possibilities in Crypto Trading!
       </div>
+              </div>
+      
     </section>
     // <SectionLayout>
     //       <TextSection />
